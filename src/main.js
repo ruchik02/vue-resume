6b1481816d5d922b6e50507fc90419cec67d7df4
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2025-03-11 18:56:36 +0530

    setup store

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import "@mdi/font/css/materialdesignicons.css";
+import store from "./store/store";
 
-const vuetify = createVuetify({ components });
+const vuetify = createVuetify({ 
+    components,
+    icons: {
+        defaultSet: 'mdi',
+    },
+
+});
 
 const app = createApp(App);
 app.use(vuetify);
+app.use(store);
 app.use(router);
 app.mount("#app");
