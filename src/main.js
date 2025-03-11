7688fb942ee3335241a5f1528b68047bd6f6b5a0
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2025-03-11 18:04:03 +0530

    set up vue-router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
-import * as components from 'vuetify/components'
-import { createApp } from 'vue'
-import App from './App.vue'
-import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+import "@mdi/font/css/materialdesignicons.css";
 
-const vuetify = createVuetify({components})
+const vuetify = createVuetify({ components });
 
-const app = createApp(App)
-app.use(vuetify)
-app.mount('#app')
+const app = createApp(App);
+app.use(vuetify);
+app.use(router);
+app.mount("#app");
